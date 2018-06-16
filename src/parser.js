a72654658b604c84a536a99d0406eb6481fb06e1
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2018-06-16 23:19:14 +0100

    Improve plainText conversion

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,8 @@
 const toPlainText = (iArr) => iArr.map(( { name, arg1, arg2 }) => {
-    return [name, arg1, arg2].filter((v) => v !== undefined).join(', ');
-}).join('\n');
+    const hexArgs = [arg1, arg2].filter(v => v !== undefined).map(v => `0x${v.toString(16)}`);
+    return [name]
+        .concat(hexArgs)
+        .join(', ');
+}).map((str, i) => `${String(i).padStart(6, 0)} ${str}`).join('\n');
 
 module.exports = { toPlainText };
--- src/parser.spec.js
+++ src/parser.spec.js
@@ -14,11 +14,30 @@ describe("parser", () => {
                     { name: "STAX D", size: 1 },
                 ];
 
-                const expected = ([ "NOP", "RRC", "STAX D" ]).join('\n');
+                const expected = ([ "000000 NOP", "000001 RRC", "000002 STAX D" ]).join('\n');
 
                 expect(toPlainText(input)).to.be.equal(expected);
             });
         });
+
+        describe('when there is one argument', () => {
+            it("returns a string with names and the first arguments only", () => {
+                const input = [
+                    { name: "INX D", size: 1, arg1: 0x10 },
+                    { name: "DCR E", size: 1, arg1: 0xf1 },
+                    { name: "DCR H", size: 1, arg1: 0xc3 },
+                ];
+
+                const expected = ([
+                    "000000 INX D, 0x10",
+                    "000001 DCR E, 0xf1",
+                    "000002 DCR H, 0xc3"
+                ]).join('\n');
+
+                expect(toPlainText(input)).to.be.equal(expected);
+            });
+        });
+
     });
 });
 
