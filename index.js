ef2afee60605e5336553416fe35e78881c176f9b
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2018-06-16 14:58:11 +0100

    Add index.js exporting modules

---
+++ index.js
@@ -0,0 +1,11 @@
+const { decode } = require('./src/decoder');
+const {
+    disassemble,
+    disassembleFile
+} = require('./src/disassembler');
+
+module.exports = {
+    decode,
+    disassemble,
+    disassembleFile
+}
