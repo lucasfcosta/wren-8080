5c71b1be1927187413296d18e1f74c08985cbf9c
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2018-06-16 14:59:08 +0100

    Add CLI tool

---
+++ cli/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const util = require('util');
+const fs = require('fs');
+const jsonexport = require('jsonexport');
+const program = require('commander');
+
+const toPlainText = (arr) => arr.map(i => i.name).join('\n')
+const toCsv = util.promisify(jsonexport)
+const writeFile = util.promisify(fs.writeFile)
+
+const { disassembleFile } = require('../src/disassembler');
+
+program
+    .option('-c, --csv <outputPath>', 'Save the results to a csv file')
+    .option('-t, --txt <outputPath>', 'Save the results to a text file')
+
+const main = async (path) => {
+    const instructions = await disassembleFile(path)
+    await output(instructions)
+    console.log('Done!'); // eslint-disable-line no-console
+}
+
+const output = async (instructions) => {
+    if (program.csv) {
+        return writeFile(program.csv, (await toCsv(instructions)))
+    } else if (program.txt) {
+        return writeFile(program.txt, (await toPlainText(instructions)))
+    }
+
+    console.log(toPlainText(instructions)); // eslint-disable-line no-console
+}
+
+program.action(main)
+
+program.parse(process.argv)
