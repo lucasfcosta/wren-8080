3ed2aec2db3a44e5b28d5600ffe878261e391e32
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2018-06-17 13:36:52 +0100

    Fix tests for parser names

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,8 @@
 const toPlainText = (iArr, opOnly) => {
     let offset = 0;
 
-    return iArr.map(( { name, size, arg1, arg2 }) => {
-        const hexArgs = [arg1, arg2].filter(v => v !== undefined).map(v => `0x${v.toString(16)}`);
+    return iArr.map(( { name, size, arg0, arg1 }) => {
+        const hexArgs = [arg0, arg1].filter(v => v !== undefined).map(v => `0x${v.toString(16)}`);
         const content = opOnly ? name : [name].concat(hexArgs).join(', ');
         const offsetStr = String(offset).padStart(6, 0);
         const line = opOnly ? content : [offsetStr, content].join(' ');
